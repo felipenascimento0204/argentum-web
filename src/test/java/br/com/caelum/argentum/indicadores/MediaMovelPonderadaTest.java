15b9ae207c2dad14cb60445b2ddb0dab20b50ee9
felipenascimento0204 <felipe.nascimento0204@gmail.com>
2018-06-13 22:45:25 -0300

    Ajustando teste quebrado

--- src/test/java/br/com/caelum/argentum/indicadores/MediaMovelPonderadaTest.java
+++ src/test/java/br/com/caelum/argentum/indicadores/MediaMovelPonderadaTest.java
@@ -16,8 +16,7 @@ public class MediaMovelPonderadaTest {
 		Assert.assertEquals(14.0/6, mmp.calcula(2,  serie), 0.00001);
 		Assert.assertEquals(20.0/6, mmp.calcula(3,  serie), 0.00001);
 		Assert.assertEquals(26.0/6, mmp.calcula(4,  serie), 0.00001);
-		//Assert.assertEquals(32.0/6, mmp.calcula(5,  serie), 0.00001);
-		Assert.assertEquals(32.0/6, mmp.calcula(4,  serie), 0.00001);
+		Assert.assertEquals(32.0/6, mmp.calcula(5,  serie), 0.00001);
 	}
 
 }
